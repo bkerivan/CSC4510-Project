c27c14539b350507d8ded60f16b5355cec3aa599
bkerivan <bkerivan@villanova.edu>
2019-11-03 21:16:59 -0500

    Add dataframe population to QuickFacts scraper

--- get_demographic_data.py
+++ get_demographic_data.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 
+#TODO: Detect invalid county name by checking for "United States" title of
+#      QuickFacts page
+
 
 import pandas as pd
 
@@ -10,6 +13,17 @@ from bs4 import BeautifulSoup
 
 
 QUICK_FACTS_URL = "https://census.gov/quickfacts"
+
+ATTRIBUTE_MAP = {
+    "race_wht_pct": "RHI125218",
+    "race_blkafr_pct": "RHI225218",
+    "race_amrindalsk_pct": "RHI325218",
+    "race_asn_pct": "RHI425218",
+    "race_hwnpcf_pct": "RHI525218",
+    "race_multi_pct": "RHI625218",
+    "race_hspltn_pct": "RHI725218",
+    "race_whtnothsp_pct": "RHI825218"
+}
 
 
 def get_county_names(data_path):
@@ -41,12 +55,18 @@ class QuickFactsScraper:
 
         html = BeautifulSoup(r.text, "lxml")
 
-        race_wht_pct_field = html.find("tr", {"data-mnemonic": "RHI125218"})
-        race_wht_pct_stat = race_wht_pct_field.findChild(attrs={"data-value": True})
-        race_wht_pct = float(race_wht_pct_stat["data-value"])
-        print(race_wht_pct)
+        attributes = {"county": county}
 
-    def close():
+        for attribute, tag in ATTRIBUTE_MAP.items():
+            attribute_field = html.find("tr", {"data-mnemonic": tag})
+            attribute_element = attribute_field.findChild(attrs={"data-value": True})
+            attribute_value = float(attribute_element["data-value"])
+            attributes[attribute] = attribute_value
+
+        record = pd.DataFrame([attributes], columns=attributes.keys())
+        self.demographic_data = self.demographic_data.append(record)
+
+    def close(self):
         self.session.close()
 
 
@@ -59,4 +79,6 @@ if __name__ == "__main__":
     
     scraper = QuickFactsScraper()
     scraper.scrape_county_data("autaugacountyalabama")
+    print(scraper.demographic_data)
+    scraper.close()
 
