eda0277a41c0adb8d7edb5e4616291717d1a3017
bkerivan <bkerivan@villanova.edu>
2019-11-04 13:17:26 -0500

    Modify demographics dataframe format

--- get_demographic_data.py
+++ get_demographic_data.py
@@ -12,9 +12,8 @@ import sys
 from bs4 import BeautifulSoup
 
 
-QUICK_FACTS_URL = "https://census.gov/quickfacts"
-
-ATTRIBUTE_MAP = {
+QUICKFACTS_URL = "https://census.gov/quickfacts"
+QUICKFACTS_ATTRIBUTE_MAP = {
     "race_wht_pct": "RHI125218",
     "race_blkafr_pct": "RHI225218",
     "race_amrindalsk_pct": "RHI325218",
@@ -26,46 +25,70 @@ ATTRIBUTE_MAP = {
 }
 
 
-def get_county_names(data_path):
+def build_quickfacts_county_map(data_path):
     election_data = pd.read_csv(data_path)
 
     # Don't know how to handle Alaska "districts" right now
     election_data = election_data[election_data["state"] != "Alaska"]
 
-    # Replace any spaces and periods per QuickFacts URL format
-    counties = [county.lower().replace(' ', '').replace('.', '') + "county" + state.lower().replace(' ', '') for county, state in zip(election_data["county"], election_data["state"])]
+    # Each state is mapped to a map of its consitituent counties
+    # Each county is mapped to its representation in QuickFacts URL format
+    county_map = {
+        state: {
+            # Remove spaces and periods per QuickFacts URL format
+            county: "{}county{}".format(county.lower().replace(' ', '').replace('.', ''), state.lower().replace(' ', ''))
+            for county in election_data[election_data["state"] == state]["county"]
+        }
+        for state in election_data["state"]
+    }
 
-    # Some misc. replacements
-    return [county if county != "districtofcolumbiacountydistrictofcolumbia" else "DC" for county in counties]
+    return county_map
+
+
+# Raised if county not found in data, or county QuickFacts parameter doesn't
+# return a QuickFacts page for that county
+class InvalidCountyException(Exception):
+    pass
 
 
 class QuickFactsScraper:
-    def __init__(self, counties=[]):
+    # Needs to be passed the county map, so that each scraper doesn't have to
+    # build from data, which is slow
+    def __init__(self, county_map):
         self.session = requests.Session()
-        self.demographic_data = pd.DataFrame(columns=["county", "race_wht_pct",
-        "race_blkafr_pct", "race_amrindalsk_pct", "race_asn_pct", "race_hwnpcf_pct",
-        "race_multi_pct", "race_hspltn_pct", "race_whtnothsp_pct"])
-        self.counties = counties
+        self.quickfacts_data = pd.DataFrame(columns=["state", "county"] + list(QUICKFACTS_ATTRIBUTE_MAP.keys()))
+        self.county_map = county_map
 
-    def scrape_county_data(self, county):
-        r = self.session.get("{}/{}".format(QUICK_FACTS_URL, county))
+    # Both state and county must be provided, so both can be dataframe
+    # attributes
+    def scrape_county_data(self, state, county):
+        try:
+            quickfacts_county = self.county_map[state][county]
+        except KeyError:
+            raise InvalidCountyException("County {}, {} not found in election data".format(county, state))
+
+        r = self.session.get("{}/{}".format(QUICKFACTS_URL, quickfacts_county))
 
         if r.status_code != 200:
             r.raise_for_status()
 
         html = BeautifulSoup(r.text, "lxml")
 
-        attributes = {"county": county}
+        # Can append dict to dataframe as a row
+        attributes = {"state": state, "county": county}
 
-        for attribute, tag in ATTRIBUTE_MAP.items():
+        # data-mnemonic identifies the attribute type in the QuickFacts table,
+        # as represented by QUICKFACTS_ATTRIBUTE_MAP
+        for attribute, tag in QUICKFACTS_ATTRIBUTE_MAP.items():
             attribute_field = html.find("tr", {"data-mnemonic": tag})
             attribute_element = attribute_field.findChild(attrs={"data-value": True})
             attribute_value = float(attribute_element["data-value"])
             attributes[attribute] = attribute_value
 
         record = pd.DataFrame([attributes], columns=attributes.keys())
-        self.demographic_data = self.demographic_data.append(record)
+        self.quickfacts_data = self.quickfacts_data.append(record)
 
+    # Not sure if this is necessary but I suppose it doesn't hurt
     def close(self):
         self.session.close()
 
@@ -75,10 +98,11 @@ if __name__ == "__main__":
         print("Usage: {} <cleaned data path>".format(sys.argv[0]))
         sys.exit(1)
 
-    counties = get_county_names(sys.argv[1])
-    
-    scraper = QuickFactsScraper()
-    scraper.scrape_county_data("autaugacountyalabama")
-    print(scraper.demographic_data)
-    scraper.close()
+    county_map = build_quickfacts_county_map(sys.argv[1])
+    scraper = QuickFactsScraper(county_map)
 
+    # Seems to work for a few different tested counties
+    scraper.scrape_county_data("Wisconsin", "Oconto")
+    print(scraper.quickfacts_data)
+    scraper.close()
+    
